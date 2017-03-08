c3ba1b1584c79817e5acc631bd9b6c3922513ed9
Amjad Hussain <amjadhussain@Amjads-MacBook-Pro.local>
2017-03-08 23:21:45 +0100

    browsersync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,38 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+
+var SOURCEPATHS = {
+    sassSource: 'src/scss/app.scss'
+}
+
+var DESTINATIONPATH = {
+    root: 'app/',
+    css: 'app/css',
+    js: 'app/js'
+}
 
 gulp.task('sass', function(){
-   return gulp.src('src/scss/app.scss')
+   return gulp.src(SOURCEPATHS.sassSource)
    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
-   .pipe(gulp.dest('app/css'));
+   .pipe(gulp.dest(DESTINATIONPATH.css));
 });
 
-gulp.task('default');
+
+gulp.task('server', ['sass'], function(){
+    browserSync.init([DESTINATIONPATH.css + '/*.css', DESTINATIONPATH.root + '/*.html', DESTINATIONPATH.js + '/*.js'], {
+        server: {
+            baseDir: DESTINATIONPATH.root
+        }
+    })
+});
+
+
+gulp.task('watch', ['server'], function(){
+   gulp.watch([SOURCEPATHS.sassSource], ['sass']) 
+});
+
+
+gulp.task('default', ['watch']);
